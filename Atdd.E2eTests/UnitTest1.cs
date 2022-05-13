1f5dcfbb03d6a0e6008f04701544e0623b3b02cd
Mark Broadhurst <Mark.Broadhurst@kantar.com>
2022-05-13 10:47:56 +0100

    Initial Code

---
+++ Atdd.E2eTests/UnitTest1.cs
@@ -0,0 +1,17 @@
+using NUnit.Framework;
+
+namespace Atdd.E2eTests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
